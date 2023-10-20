887360aabc013f15df11de85dd1af6dc66b4b097
Marnien Cueba <marniencueba94@gmail.com>
2023-10-21 02:37:56 +0800

    shared enums

---
+++ src/shared/enum.ts
@@ -0,0 +1,21 @@
+export enum ColorEnum {
+  primary,
+  secondary,
+  info,
+  success,
+  warning,
+  danger,
+  default,
+  black,
+  white,
+}
+
+export enum DefaultColorEnum {
+  primary = ColorEnum.primary,
+  secondary = ColorEnum.secondary,
+  info = ColorEnum.info,
+  success = ColorEnum.success,
+  warning = ColorEnum.warning,
+  danger = ColorEnum.danger,
+  default = ColorEnum.default,
+}
