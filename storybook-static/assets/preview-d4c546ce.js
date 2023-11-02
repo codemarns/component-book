9f358f06fb640ea0aa7ebdca1c51d5dd67866c02
Marnien Cueba <marniencueba94@gmail.com>
2023-11-02 15:03:20 +0800

    storybook build

--- storybook-static/assets/preview-d4c546ce.js
+++ storybook-static/assets/preview-d4c546ce.js
@@ -1,2 +1,2 @@
 const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},options:{storySort:{order:["Welcome","Design System","Components","Utilities","*"]}}}};export{e as default};
-//# sourceMappingURL=preview-4a853f9a.js.map
+//# sourceMappingURL=preview-d4c546ce.js.map
